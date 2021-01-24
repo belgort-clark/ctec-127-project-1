ebb855842d6d6e603ea407c396cb44e6ea3c49bf
belgort-clark <belgort@clark.edu>
2021-01-24 11:27:39 -0800

    Initial commit

---
+++ process_multiple_forms.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Multiple Form Processor</title>
+</head>
+
+<body>
+    <!-- Your code goes here. This comment must be removed. -->
+</body>
+
+</html>
