d802ba2b564667c25a1ae48ee86c43d4feefdef2
Bruce Elgort <bruce.elgort@gmail.com>
2022-01-22 07:09:18 -0800

    Updated for Winter 2022

--- ctec-127-project-1.php
+++ ctec-127-project-1.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Project 1</title>
+    <title>CTEC 127 - Project 1</title>
 </head>
 
 <body>
