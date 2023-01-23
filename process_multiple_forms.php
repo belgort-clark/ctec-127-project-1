62c599fabfd04aaddecf326b2fcac9a751e74d83
Bruce Elgort <bruce.elgort@gmail.com>
2023-01-22 18:56:43 -0800

    Winter 2023

--- process_multiple_forms.php
+++ process_multiple_forms.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>CTEC 127 - Multiple Form Processor</title>
+    <title>CTEC 127 - Multiple Form Processor 2023</title>
 </head>
 
 <body>
